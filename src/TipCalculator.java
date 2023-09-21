38f686fe56cf26ebd68788fc8fd2a3a798b93f3d
BT_1N3_29 <sample@bths.edu>
2023-09-21 10:18:22 -0400

    asking user to input the price of their food and starting a while loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args){
         Scanner scan = new Scanner (System.in);
-        System.out.println("Welcome to the tip calculator program! How many people will be seated today? ");
+        System.out.println("Welcome to the tip calculator program!");
+        System.out.println("How many people will be seated today? ");
         int people = scan.nextInt();
 
-        System.out.println("Enter the tip percentage as an integer (without the percentage sign): ");
+        System.out.println("What's the tip percentage? (0-100): ");
         int tip = scan.nextInt();
+
+        System.out.print("Enter a cost in dollars and cents, e.g. 12.50 )-1 to end): ");
+        double num = scan.nextDouble();
+
+        while (num != -1) {
+
+
+        }
 
 
 
