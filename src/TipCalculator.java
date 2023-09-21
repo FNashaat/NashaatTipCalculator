7edd3928fceb31c9ad294ef316ce674abc1b3ab6
BT_1N3_29 <sample@bths.edu>
2023-09-21 10:08:59 -0400

    initial commit: welcoming user, asking for number of people, and their tip percentage

---
+++ src/TipCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Welcome to the tip calculator program! How many people will be seated today? ");
+        int people = scan.nextInt();
+
+        System.out.println("Enter the tip percentage as an integer (without the percentage sign): ");
+        int tip = scan.nextInt();
+
+
+
+
+
+    }
+}
